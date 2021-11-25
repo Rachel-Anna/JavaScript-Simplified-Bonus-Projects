a680d647ea7b6864ea2c463eb7f61bc40f226ba9
Rochie424 <67237482+Rochie424@users.noreply.github.com>
2021-11-25 10:24:37 +0100

    finished

--- toolip/before/tooltip.js
+++ toolip/before/tooltip.js
@@ -10,6 +10,15 @@ document.body.append(toolTipContainer);
 
 const DEFAULT_SPACING = 5;
 
+let POSITION_ORDER = ["top", "right", "bottom", "left"];
+
+let MAP_POSITIONS_TO_FUNCTIONS = {
+  top: positionTooltipTop,
+  right: positionTooltipRight,
+  bottom: positionTooltipBottom,
+  left: positionTooltipLeft,
+};
+
 addGlobalEventListener("mouseover", "[data-tooltip]", (e) => {
   const tooltipMessage = createTooltipMessage(e.target.dataset.tooltip);
   e.target.addEventListener(
@@ -20,6 +29,7 @@ addGlobalEventListener("mouseover", "[data-tooltip]", (e) => {
     { once: true }
   );
   toolTipContainer.append(tooltipMessage);
+
   positionTooltip(tooltipMessage, e.target);
 });
 
@@ -36,13 +46,160 @@ function removeTooltip(tooltipMessage) {
 }
 
 function positionTooltip(tooltipMessage, element) {
-  const tooltipMessageInfo = tooltipMessage.getBoundingClientRect();
-  const elementInfo = element.getBoundingClientRect();
+  const tooltipMessageRect = tooltipMessage.getBoundingClientRect();
+  const elementRect = element.getBoundingClientRect();
   const spacing = element.dataset.spacing || DEFAULT_SPACING;
+  const preferedPositions = (element.dataset.positions || "").split("|");
+  //console.log(preferedPositions);
+  const positions = preferedPositions.concat(POSITION_ORDER);
+
+  for (i = 0; i < positions.length; i++) {
+    const func = MAP_POSITIONS_TO_FUNCTIONS[positions[i]];
+
+    console.log(positions.length);
+    if (func && func(tooltipMessage, spacing, elementRect, tooltipMessageRect))
+      return;
+  }
+}
+
+function positionTooltipTop(
+  tooltipMessage,
+  spacing,
+  elementRect,
+  tooltipMessageRect
+) {
+  console.log("her");
   tooltipMessage.style.top = `${
-    elementInfo.top - tooltipMessageInfo.height - spacing
+    elementRect.top - tooltipMessageRect.height - spacing
   }px`;
+  tooltipMessage.style.backgroundColor = "green";
+
   tooltipMessage.style.left = `${
-    elementInfo.left + elementInfo.width / 2 - tooltipMessageInfo.width / 2
+    elementRect.left + elementRect.width / 2 - tooltipMessageRect.width / 2
   }px`;
+
+  const bounds = isOutOfBounds(tooltipMessage, spacing);
+  if (bounds.top) {
+    resetTooltipPosition(tooltipMessage);
+    return false;
+  }
+
+  if (bounds.right) {
+    tooltipMessage.style.right = `${spacing}px`;
+    tooltipMessage.style.left = "initial";
+  }
+  if (bounds.left) {
+    tooltipMessage.style.left = `${spacing}px`;
+  }
+  return true;
+}
+
+function positionTooltipBottom(
+  tooltipMessage,
+  spacing,
+  elementRect,
+  tooltipMessageRect
+) {
+  tooltipMessage.style.top = `${elementRect.bottom + spacing}px`;
+  tooltipMessage.style.backgroundColor = "blue";
+  tooltipMessage.style.left = `${
+    elementRect.left + elementRect.width / 2 - tooltipMessageRect.width / 2
+  }px`;
+
+  const bounds = isOutOfBounds(tooltipMessage, spacing);
+
+  if (bounds.bottom) {
+    resetTooltipPosition(tooltipMessage);
+    return false;
+  }
+
+  if (bounds.right) {
+    tooltipMessage.style.right = `${spacing}px`;
+    tooltipMessage.style.left = "initial";
+  }
+  if (bounds.left) {
+    tooltipMessage.style.left = `${spacing}px`;
+  }
+  return true;
+}
+
+function positionTooltipLeft(
+  tooltipMessage,
+  spacing,
+  elementRect,
+  tooltipMessageRect
+) {
+  tooltipMessage.style.top = `${
+    elementRect.top + elementRect.height / 2 - tooltipMessageRect.height / 2
+  }px`;
+  tooltipMessage.style.backgroundColor = "purple";
+  tooltipMessage.style.left = `${
+    elementRect.left - tooltipMessageRect.width - spacing
+  }px`;
+
+  const bounds = isOutOfBounds(tooltipMessage, spacing);
+
+  if (bounds.left) {
+    resetTooltipPosition(tooltipMessage);
+    return false;
+  }
+  if (bounds.top) {
+    tooltipMessage.style.top = `${spacing}px`;
+  }
+
+  if (bounds.bottom) {
+    tooltip.style.bottom = `${spacing}px`;
+    tooltip.style.top = "initial";
+  }
+
+  return true;
+}
+
+function positionTooltipRight(
+  tooltipMessage,
+  spacing,
+  elementRect,
+  tooltipMessageRect
+) {
+  tooltipMessage.style.top = `${
+    elementRect.top + elementRect.height / 2 - tooltipMessageRect.height / 2
+  }px`;
+  tooltipMessage.style.backgroundColor = "red";
+  console.log(spacing);
+  tooltipMessage.style.left = `${elementRect.right + spacing}px`;
+
+  const bounds = isOutOfBounds(tooltipMessage, spacing);
+  console.log(bounds);
+  if (bounds.right) {
+    resetTooltipPosition(tooltipMessage);
+    return false;
+  }
+  if (bounds.top) {
+    tooltipMessage.style.top = `${spacing}px`;
+  }
+  if (bounds.bottom) {
+    tooltipMessage.style.bottom = `${spacing}px`;
+    tooltipMessage.style.top = "initial";
+  }
+
+  return true;
+}
+
+function resetTooltipPosition(tooltipMessage) {
+  tooltipMessage.style.left = "initial";
+  tooltipMessage.style.right = "initial";
+  tooltipMessage.style.top = "initial";
+  tooltipMessage.style.bottom = "initial";
+}
+
+function isOutOfBounds(element, spacing) {
+  const rect = element.getBoundingClientRect();
+  const containerRect = toolTipContainer.getBoundingClientRect();
+
+  return {
+    left: rect.left <= containerRect.left + spacing,
+    right: rect.right >= containerRect.right - spacing,
+    top: rect.top <= containerRect.top + spacing,
+    bottom: rect.bottom >= containerRect.bottom - spacing,
+  };
 }
